0ad3590babe821651d692aadf5c4de4902bcc14f
Mateusz Urbańczyk <mu@qed.ai>
2021-10-20 07:51:14 +0200

    Rename project to original template_cpp

--- docker/run.sh
+++ docker/run.sh
@@ -3,6 +3,6 @@ set -e
 
 docker run \
     --rm \
-    --volume "/home/mu/EPFL/DA/project-repo/project:/app" \
+    --volume "/home/mu/EPFL/DA/project-repo/template_cpp:/app" \
     -it da_image \
     /bin/bash
